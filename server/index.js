ea1d6df8916a03fd0170e7a04e642552e6881966
Anurag Verma <mindreaders7557@gmail.com>
2025-11-29 19:13:07 +0530

    profiles

--- server/index.js
+++ server/index.js
@@ -90,7 +90,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Profile-ID']
 }));
 
 // Session middleware (required for Passport)
